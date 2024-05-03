8437280b2bf2f00f2f3b3981fc4a0eae842e3977
lemoonchild <ncastrom21@gmail.com>
2024-05-02 22:01:41 -0600

    feat: added the components for the blog

---
+++ my-evangelion-blog/src/components/calendar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css'
+
+import './calendar.css'
+
+const MyCalendar = () => {
+  return <Calendar />
+}
+
+export default MyCalendar
