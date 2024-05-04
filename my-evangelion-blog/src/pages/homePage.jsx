c9e3e3bee0658eb7bca618c69eb067abd729ea54
lemoonchild <ncastrom21@gmail.com>
2024-05-03 20:36:06 -0600

    feat: added more elements

--- my-evangelion-blog/src/pages/homePage.jsx
+++ my-evangelion-blog/src/pages/homePage.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './homePage.css'
 import MyCalendar from '../components/calendar.jsx'
 import GifSlider from '../components/gifSlider.jsx'
 import RandomStuffSlider from '../components/randomStuffSlider.jsx'
 
+import { Home, About, Posts } from '../pages/centerPages.jsx'
+
 const Blog = () => {
   const username = 'Lemoonchild'
+
+  const [activeTab, setActiveTab] = useState('home')
+
+  const handleNavClick = (tab) => {
+    setActiveTab(tab)
+  }
+
   return (
     <div className="blog">
       <div className="all__blog">
@@ -39,35 +48,28 @@ const Blog = () => {
         <div className="main">
           <div className="left">
             <div className="nav">
-              <div className="title__left__bar">
+              <div className="title__bar">
                 <h1>nav</h1>
               </div>
-
               <div className="nav__elements">
-                <p className="element">
-                  <Link to="/login">
-                    <span className="icon__home"></span> home
-                  </Link>
+                <p className="element" onClick={() => handleNavClick('home')}>
+                  <span className="icon__home"></span> home
                 </p>
-                <p className="element">
-                  <Link to="/login">
-                    <span className="icon__posts"></span>posts
-                  </Link>
+                <p className="element" onClick={() => handleNavClick('posts')}>
+                  <span className="icon__posts"></span>posts
                 </p>
-                <p className="element">
-                  <Link to="/login">
-                    <span className="icon__me"></span>about me!
-                  </Link>
+                <p className="element" onClick={() => handleNavClick('about')}>
+                  <span className="icon__me"></span>about me!
                 </p>
               </div>
             </div>
             <GifSlider />
             <div className="bio">
-              <div className="title__left__bar">
+              <div className="title__bar">
                 <h1>bio</h1>
               </div>
 
-              <div className="nav__elements">
+              <div className="elements">
                 <p>
                   <span className="icon__star"> </span>learn more about me:
                 </p>
@@ -82,24 +84,27 @@ const Blog = () => {
             </div>
 
             <div className="calendar">
-              <div className="title__left__bar">
+              <div className="title__bar">
                 <h1>calendar</h1>
               </div>
               <MyCalendar className="calendar__blog" />
             </div>
+            <img
+              src="https://media.tenor.com/kKCY4vdS6s0AAAAi/evangelion-eva01.gif"
+              alt="rei"
+              className="rei"
+            />
           </div>
 
           <div className="center">
-            <div className="welcome"></div>
-
-            <div className="links"></div>
-
-            <div className="home__page"></div>
+            {activeTab === 'home' && <Home />}
+            {activeTab === 'about' && <About />}
+            {activeTab === 'posts' && <Posts />}
           </div>
 
           <div className="right">
-            <div className="nav">
-              <div className="title__left__bar">
+            <div className="status">
+              <div className="title__bar">
                 <h1>status</h1>
               </div>
 
@@ -108,23 +113,40 @@ const Blog = () => {
                 <span>♡ 02/05/2024:</span> <p>hate traffic</p>
                 <span>♡ 02/05/2024:</span> <p>feels like saturday</p>
                 <span>♡ 02/05/2024:</span> <p>this blog is killing me</p>
+                <span>♡ 03/05/2024:</span> <p>want to play dmc5</p>
+                <span>♡ 03/05/2024:</span> <p>in my bf house</p>
+                <span>♡ 03/05/2024:</span> <p>i hate virtual classes</p>
               </div>
             </div>
 
             <RandomStuffSlider />
-            <div className="bio">
-              <div className="title__left__bar">
-                <h1>to-do list</h1>
+            <div className="find__me">
+              <div className="title__bar">
+                <h1>my socials!</h1>
               </div>
 
               <div className="nav__elements">
-                <p>Learn more about me</p>
-                <p>Hello!</p>
+                <p className="element">
+                  <span className="icon__steam"></span>
+                  <a href="https://steamcommunity.com/profiles/76561199202483759/">steam!</a>
+                </p>
+                <p className="element">
+                  <span className="icon__github"></span>
+                  <a href="https://github.com/lemoonchild">github!</a>
+                </p>
+                <p className="element">
+                  <span className="icon__twitter"></span>
+                  <a href="https://twitter.com/lemoonchildd">twitter!</a>
+                </p>
+                <p className="element">
+                  <span className="icon__insta"></span>
+                  <a href="https://www.instagram.com/limoonchildd/?hl=es">insta!</a>
+                </p>
               </div>
             </div>
 
-            <div className="calendar">
-              <div className="title__left__bar">
+            <div className="my__cat">
+              <div className="title__bar">
                 <h1>my cat</h1>
               </div>
               <img src="../../images/cat.png" alt="cat" className="cat" />
@@ -137,7 +159,9 @@ const Blog = () => {
           </div>
         </div>
 
-        <div className="footer"></div>
+        <div className="footer">
+          <p>this is the footer</p>
+        </div>
       </div>
     </div>
   )
