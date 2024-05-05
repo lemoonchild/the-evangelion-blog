0f9581a87a339b24133c16c28dcd63d81a3a7202
lemoonchild <ncastrom21@gmail.com>
2024-05-04 20:03:11 -0600

    feat: complete the elements for the button

--- my-evangelion-blog/src/components/button.jsx
+++ my-evangelion-blog/src/components/button.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './button.css'
 
-const Button = ({ text, onClick, className }) => {
+const Button = ({ onClick, text }) => {
+  const splitText = (text) => {
+    return text.split('').map((char, index) => (
+      <span key={index} style={{ '--char-index': index }} className="char">
+        {char}
+      </span>
+    ))
+  }
+
   return (
-    <button className={`button ${className}`} onClick={onClick}>
-      {text}
+    <button onClick={onClick} className="button">
+      {splitText(text)}
     </button>
   )
 }
