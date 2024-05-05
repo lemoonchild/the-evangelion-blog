53aa471b9eb80b1b79f104e155f7c27ea2f33988
lemoonchild <ncastrom21@gmail.com>
2024-05-05 17:43:59 -0600

    feat: added the auth of the users in the Router

--- my-evangelion-blog/src/App.jsx
+++ my-evangelion-blog/src/App.jsx
@@ -2,18 +2,22 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { Login, Register } from '../src/pages/loginRegister.jsx'
 import BlogMain from './pages/homePage'
+import { AuthProvider } from '../src/hooks/autProvider.jsx'
+
 import './App.css'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/login" />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/blog" element={<BlogMain action="blog" />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/login" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/blog" element={<BlogMain action="blog" />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   )
 }
 
