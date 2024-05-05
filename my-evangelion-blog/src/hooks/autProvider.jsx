ffee2de63a3348fac988b0956cbb1c3c033e29e7
lemoonchild <ncastrom21@gmail.com>
2024-05-05 17:43:35 -0600

    feat: added the auth of the user

---
+++ my-evangelion-blog/src/hooks/autProvider.jsx
@@ -0,0 +1,41 @@
+import React, { createContext, useContext, useState, useEffect } from 'react'
+
+const AuthContext = createContext(null)
+
+export const useAuth = () => useContext(AuthContext)
+
+export const AuthProvider = ({ children }) => {
+  const [authData, setAuthData] = useState({ token: null, user: null })
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    const userJson = localStorage.getItem('user')
+    try {
+      const user = userJson ? JSON.parse(userJson) : null
+      setAuthData({ token, user })
+    } catch (error) {
+      console.error('Failed to parse user data:', error)
+    }
+  }, [])
+
+  const login = (token, user) => {
+    localStorage.setItem('token', token)
+    localStorage.setItem('user', JSON.stringify(user))
+    setAuthData({ token, user })
+  }
+
+  const logout = () => {
+    localStorage.removeItem('token')
+    localStorage.removeItem('user')
+    setAuthData({ token: null, user: null })
+  }
+
+  const authContextValue = {
+    authToken: authData.token,
+    user: authData.user,
+    login,
+    logout,
+  }
+
+  return <AuthContext.Provider value={authContextValue}>{children}</AuthContext.Provider>
+}
